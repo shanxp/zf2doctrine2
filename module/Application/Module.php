34fa7b98e869d5bf72d45d01ba64275859b14fad
Shan Shanmugaesan <shan.shanmugaesan@sqsltd.co.uk>
2013-09-11 17:37:19 +0100

    Add array-based ACL implementation

--- module/Application/Module.php
+++ module/Application/Module.php
@@ -3,6 +3,7 @@
 namespace Application;
 
 use Zend\Mvc\ModuleRouteListener;
+use Zend\Mvc\MvcEvent ;
 
 class Module
 {
@@ -12,6 +13,8 @@ class Module
         $eventManager        = $e->getApplication()->getEventManager();
         $moduleRouteListener = new ModuleRouteListener();
         $moduleRouteListener->attach($eventManager);
+        $this->initAcl($e);
+        $eventManager->attach('route' , array($this, 'checkAcl') );
     }
 
     public function getConfig()
@@ -29,5 +32,54 @@ class Module
             ),
         );
     }
+    
+    
+    public function initAcl(MvcEvent $e) 
+    {
+
+        $acl = new \Zend\Permissions\Acl\Acl();
+        $roles = include __DIR__ . '/config/module.acl.roles.php';
+        $allResources = array();
+        foreach ($roles as $role => $resources) {
+
+            $role = new \Zend\Permissions\Acl\Role\GenericRole($role);
+            $acl -> addRole($role);
+
+            $allResources = array_merge($resources, $allResources);
+
+            //adding resources
+            foreach ($resources as $resource) {
+                 // Edit 4
+                 if(!$acl ->hasResource($resource))
+                    $acl -> addResource(new \Zend\Permissions\Acl\Resource\GenericResource($resource));
+            }
+            //adding restrictions
+            foreach ($resources as $resource) {
+                $acl -> allow($role, $resource);
+            }
+        }
+        //testing
+//        var_dump($acl->isAllowed('admin','home'));
+        //true
+
+        //setting to view
+        $e -> getViewModel()->acl = $acl;
+
+    }   
+    
+    public function checkAcl(MvcEvent $e) 
+    {
+        $route = $e -> getRouteMatch() -> getMatchedRouteName();
+        //you set your role
+        $userRole = 'guest';
+
+        if ($e -> getViewModel() -> acl ->hasResource($route) && !$e -> getViewModel() -> acl -> isAllowed($userRole, $route)) {
+            $response = $e -> getResponse();
+            //location to page or what ever
+            $response -> getHeaders() -> addHeaderLine('Location', $e -> getRequest() -> getBaseUrl() . '/404');
+            $response -> setStatusCode(303);
+
+        }
+    }    
 
 }
---
+++ module/Application/config/module.acl.roles.php
@@ -0,0 +1,10 @@
+<?php
+//tutotial : http://ivangospodinow.com/zend-framework-2-acl-setup-in-5-minutes-tutorial/
+return array(
+    'guest'=> array(
+        'album'
+    ),
+    'admin'=> array(
+        'album',
+    ),
+);
